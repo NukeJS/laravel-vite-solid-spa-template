0f45f71e88fc60ea0d14700d716d28210ebe3c11
NukeJS <rwaebers@gmail.com>
2022-08-21 16:40:20 +0200

    Update to use laravel-vite-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,14 @@
-import { defineConfig, Plugin } from "vite";
+import { defineConfig } from "vite";
 
+import laravel from 'laravel-vite-plugin'
 import solid from "vite-plugin-solid";
 
-// @ts-ignore
-export default defineConfig(({ command }) => {
-    const IS_DEV = command === "serve";
-
-    return {
-        target: "esnext",
-        base: IS_DEV ? "" : "/build/",
-        publicDir: "non_existent_folder",
-        build: {
-            outDir: "public/build",
-            manifest: true,
-            rollupOptions: {
-                input: ["resources/js/main.tsx", "resources/css/app.css"],
-            },
-        },
-        plugins: [solid()],
-    };
+export default defineConfig({
+    plugins: [
+        laravel([
+            'resources/css/app.css',
+            'resources/js/main.tsx'
+        ]),
+        solid()
+    ],
 });
